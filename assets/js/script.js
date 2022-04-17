d1d1828be9b8f9def7a313d9b547a40a410fde7f
hvphan <hvphan1993@gmail.com>
2022-04-17 02:28:32 -0700

    UX enhancements

--- assets/js/script.js
+++ assets/js/script.js
@@ -55,13 +55,13 @@ $(".list-group").on("click", "p", function () {
 var auditTask = function(taskEl) {
   // get date from task element
   var date = $(taskEl).find("span").text().trim();
-
+  
   // convert to moment object at 5:00pm
   var time = moment(date, "L").set("hour", 17);
   
   // remove any old classes from element
   $("taskEl").removeClass("list-group-item-warning list-group-item-danger");
-
+  
   //apply new class if task is near/over due date
   if (moment().isAfter(time)) {
     $("taskEl").addClass("list-group-item-danger");
@@ -159,7 +159,7 @@ $("#task-form-modal").on("shown.bs.modal", function () {
 });
 
 // save button in modal was clicked
-$("#task-form-modal .btn-primary").click(function () {
+$("#task-form-modal .btn-save").click(function () {
   // get form values
   var taskText = $("#modalTaskDescription").val();
   var taskDate = $("#modalDueDate").val();
@@ -198,15 +198,23 @@ $(".card .list-group").sortable({
   tolerance: "pointer",
   helper: "clone",
   activate: function (event) {
+    $(this).addClass("dropover");
+    $("bottom-trash").addClass("bottom-trash-drag");
     console.log("activate", this);
   },
   deactivate: function (event) {
+    $(this).removeClass("dropover");
+    $("bottom-trash").removeClass("bottom-trash-drag");
     console.log("deactivate", this);
   },
   over: function (event) {
+    $(event.target).addClass("dropover-active");
+    $("bottom-trash").addClass("bottom-trash-active");
     console.log("over", event.target);
   },
   out: function (event) {
+    $(event.target).removeClass("dropover-active");
+    $("bottom-trash").removeClass("bottom-trash-active");
     console.log("out", event.target);
   },
   update: function (event) {
@@ -257,3 +265,9 @@ $("#trash").droppable({
 $("#modalDueDate").datepicker({
   minDate: 1
 });
+
+setInterval(function () {
+  $(".card .list-group-item").each(function(index, el) {
+    auditTask(el);
+  });
+}, (1000 * 60) * 30);
